04593a9e6ed6961a38c4711afd1b9fcab9205efd
Rapha <Rapha@PcdoRapha>
2022-08-01 16:55:20 -0300

    Finalizando modulo7

--- CursoJava/src/Modulo7/Aulas/AulaAtual.java
+++ CursoJava/src/Modulo7/Aulas/AulaAtual.java
@@ -2,6 +2,8 @@ package Modulo7.Aulas;
 
 public class AulaAtual {
 	public static void main(String[] args) {
-		
+		int numero = 1;
+		String saida = "Saida " + numero;
+		System.out.println(saida);
 	}
 }
