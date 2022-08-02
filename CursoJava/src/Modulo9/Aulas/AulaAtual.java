9bf4960545bf57c36c410dc2d6a03593cad6d07b
Rapha <Rapha@PcdoRapha>
2022-08-02 18:02:55 -0300

    Aula 9.10

--- CursoJava/src/Modulo9/Aulas/AulaAtual.java
+++ CursoJava/src/Modulo9/Aulas/AulaAtual.java
@@ -4,7 +4,9 @@ import Modulo9.classes.Aluno;
 
 public class AulaAtual {
 	public static void main(String[] args) {
-		Aluno aluno1 = new Aluno();		
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Joao da Silva");
+		System.out.println("Nome é " + aluno1.getNome());
 		Aluno aluno2 = new Aluno();	
 		Aluno aluno3 = new Aluno();
 		Aluno aluno4 = new Aluno("Maria");
--- CursoJava/src/Modulo9/classes/Aluno.java
+++ CursoJava/src/Modulo9/classes/Aluno.java
@@ -1,16 +1,17 @@
 package Modulo9.classes;
 
 public class Aluno {
-	String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
+	//atributos do aluno
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
 	//padrao
 	public Aluno() {
 		
@@ -23,5 +24,14 @@ public class Aluno {
 	public Aluno (String nomePadrao, int idadePadrao) {
 		nome = nomePadrao;
 		idade = idadePadrao;
+	}
+	//SET
+	//this. faz referencia ao atributo da classe
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	//GET
+	public String getNome() {
+		return nome;
 	}
 }
