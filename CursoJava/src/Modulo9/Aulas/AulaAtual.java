662be472172f529db7c4cfcab82e4ca632eea398
Rapha <Rapha@PcdoRapha>
2022-08-02 18:57:53 -0300

    Aula 9.12

--- CursoJava/src/Modulo9/Aulas/AulaAtual.java
+++ CursoJava/src/Modulo9/Aulas/AulaAtual.java
@@ -7,6 +7,13 @@ public class AulaAtual {
 		Aluno aluno1 = new Aluno();
 		aluno1.setNome("Joao da Silva");
 		System.out.println("Nome é " + aluno1.getNome());
+		aluno1.setNota1(20);
+		aluno1.setNota2(60);
+		aluno1.setNota3(70);
+		aluno1.setNota4(90);
+		System.out.println("A média do aluno é: " + aluno1.getMediaNota());
+		System.out.println("Resultado: " + (aluno1.getAlunoAprovado()? "Aprovado" : "Reprovado"));
+		//System.out.println("Resultado pelo método 2: " + aluno1.getAlunoAprovado2());
 		Aluno aluno2 = new Aluno();	
 		Aluno aluno3 = new Aluno();
 		Aluno aluno4 = new Aluno("Maria");
--- CursoJava/src/Modulo9/classes/Aluno.java
+++ CursoJava/src/Modulo9/classes/Aluno.java
@@ -34,4 +34,55 @@ public class Aluno {
 	public String getNome() {
 		return nome;
 	}
+	//metodo media de notas
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+	//metodo que retorna a media
+	public double getMediaNota() {
+		return (nota1+nota2+nota3+nota4)/4;
+	}
+	//metodo aprovado 1 (mais utilizado pela praticidade - a responsabilidade da String fica para a tela)
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if(media>=70) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	//metodo aprovado 2
+	public String getAlunoAprovado2() {
+		double media = this.getMediaNota();
+		if(media>=70) {
+			return "Aluno aprovado";
+		}else {
+			return "Aluno reprovado";
+		}
+	}
 }
