12aa38811e7e29ec5af9b447f970449bb881c946
Rapha <Rapha@PcdoRapha>
2022-08-02 16:55:02 -0300

    Pausa aula 9.6

---
+++ CursoJava/src/Modulo9/classes/Aluno.java
@@ -0,0 +1,14 @@
+package Modulo9.classes;
+
+public class Aluno {
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+}
