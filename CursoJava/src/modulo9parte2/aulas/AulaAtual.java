f5943d0f4a070e8566ddf9c89408a66080125b54
Rapha <Rapha@PcdoRapha>
2022-08-03 17:05:33 -0300

    Aula 9.20

--- CursoJava/src/modulo9parte2/aulas/AulaAtual.java
+++ CursoJava/src/modulo9parte2/aulas/AulaAtual.java
@@ -2,6 +2,7 @@ package modulo9parte2.aulas;
 
 import javax.swing.JOptionPane;
 
+//importando esta classe aluno para diferenciar da outra
 import modulo9parte2.classes.Aluno;
 
 public class AulaAtual {
