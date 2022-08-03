579a6711cdc30c31204e32ff5f153bc59bedbc87
Rapha <Rapha@PcdoRapha>
2022-08-03 17:03:39 -0300

    Aula 9.21

--- CursoJava/src/modulo9parte2/aulas/AulaAtual.java
+++ CursoJava/src/modulo9parte2/aulas/AulaAtual.java
@@ -1,5 +1,10 @@
 package modulo9parte2.aulas;
 
-public class AulaAtual {
+import javax.swing.JOptionPane;
 
+import modulo9parte2.classes.Aluno;
+
+public class AulaAtual {
+	Aluno aluno1 = new Aluno();
+	
 }
---
+++ CursoJava/src/modulo9parte2/classes/Aluno.java
@@ -0,0 +1,101 @@
+package modulo9parte2.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aluno {
+	//atributos do aluno
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	//Lista disciplina dentro do objeto aluno
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+	//set e get da lista
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+	//padrao
+	public Aluno() {
+		
+	}
+	//1 parametro
+	public Aluno (String nomePadrao) {
+		nome = nomePadrao;
+	}
+	//2 parametro
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+	//getters e setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+}
